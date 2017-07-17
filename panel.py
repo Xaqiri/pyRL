46a8b392172e04d0abb87d6f9b0c189ce5d3881d
Zachary Robbins <xaqiri@outlook.com>
2017-07-17 10:04:37 -0500

    Changed panel to take a size tuple rather than separate width and height

--- panel.py
+++ panel.py
@@ -1,16 +1,14 @@
 import pygame as p
 
 class Panel():
-    def __init__(self, origin=(0, 0), width=0, height=0, bg_color=None, fg_color=None, screen=None, visible=False, elements=[], name=''):
+    def __init__(self, origin=(0, 0), size=(0, 0), bg_color=None, fg_color=None, screen=None, visible=False, elements=[], name=''):
         self.origin = origin
-        self.width = width
-        self.height = height
-        self.size = (self.width, self.height)
+        self.size = size
         self.bg_color = bg_color
         self.fg_color = fg_color
         self.screen = screen
         self.elements = elements
-        self.center = (self.origin[0]+self.width//2, self.origin[1]+self.height//2)
+        self.center = (self.origin[0]+self.size[0]//2, self.origin[1]+self.size[1]//2)
         self.visible = visible
         self.name = name
 
